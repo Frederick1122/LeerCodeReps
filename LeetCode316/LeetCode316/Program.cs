7210fc71a60707aad1762b4b60ec8cb5baa3da26
zaletovfyodor <zaletovfyodor@gmail.com>
2023-09-26 19:43:31 +0300

    Added daily problems

---
+++ LeetCode316/LeetCode316/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode316 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicateLetters("cbacdcbc"));
+        }
+        
+        public static string RemoveDuplicateLetters(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            HashSet<char> seen = new HashSet<char>();
+            Dictionary<char, int> lastOcc = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++) {
+                lastOcc[s[i]] = i;
+            }
+        
+            for (int i = 0; i < s.Length; i++) {
+                char c = s[i];
+                if (!seen.Contains(c)) {
+                    while (stack.Count > 0 && c < stack.Peek() && i < lastOcc[stack.Peek()]) {
+                        seen.Remove(stack.Pop());
+                    }
+                    seen.Add(c);
+                    stack.Push(c);
+                }
+            }
+        
+            char[] result = stack.ToArray();
+            Array.Reverse(result);
+            return new string(result);
+        }
+    }
+}
