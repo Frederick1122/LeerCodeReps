fe3876f08d7192b177d1f7a7437c0e44a84a8b81
zaletovfyodor <zaletovfyodor@gmail.com>
2023-09-27 20:49:30 +0300

    Added daily problem (880)

---
+++ LeetCode880/LeetCode880/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LeetCode880 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DecodeAtIndex("y959q969u3hb22odq595", 222280369));
+        }
+        
+        public static string DecodeAtIndex(string s, int k)
+        {
+            long length = 0;
+            int i = 0;
+
+            while (length < k) {
+                if (char.IsDigit(s[i])) {
+                    length *= s[i] - '0';
+                } else {
+                    length++;
+                }
+                i++;
+            }
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (char.IsDigit(s[j])) {
+                    length /= s[j] - '0';
+                    k %= (int)length;
+                } else {
+                    if (k == 0 || k == length) {
+                        return s[j].ToString();
+                    }
+                    length--;
+                }
+            }
+
+            return "";
+        }
+    }
+}
